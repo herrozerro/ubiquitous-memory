845baf10ec3aaf85fce2c18db837a72a18ceb851
Josiah Bradbury <herrozerro@gmail.com>
2019-04-20 18:17:38 -0500

    Renamed file

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -17,7 +17,7 @@ var filesToCache = [
     '/_framework/blazor.server.js',
     //Our additional files
     '/manifest.json',
-    'https://josiahbradbury.info/ubiquitous-memory/serviceworker.js',
+    '/serviceworker.js',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png',
     //Content

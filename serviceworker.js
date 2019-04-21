a3ad379a40e5c8298190b3790799a6deb024adb4
Josiah Bradbury <herrozerro@gmail.com>
2019-04-21 00:28:07 -0500

    hopefully added logging

--- serviceworker.js
+++ serviceworker.js
@@ -57,7 +57,12 @@ self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(cacheName).then(function (cache) {
             console.log('[ServiceWorker] Caching app shell');
-            return cache.addAll(filesToCache);
+            return cache.addAll(filesToCache)
+			.then(function(out){
+				console.log(out);
+			}).catch(function(err){
+				console.log(err);
+			});
         })
     );
 });

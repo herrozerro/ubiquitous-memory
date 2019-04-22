9e159d309707568192fab0c7bb763e1ab9f118fd
Josiah Bradbury <herrozerro@gmail.com>
2019-04-21 22:48:51 -0500

    removed service workers

--- serviceworker.js
+++ serviceworker.js
@@ -1,55 +1,55 @@
 ﻿console.log("This is service worker talking");
 var cacheName = 'blazor-pwa-sample';
 var filesToCache = [
-    './',
+    '/',
     //Html and css files
-    './index.html',
-    './css/site.css',
-    './css/bootstrap/bootstrap.min.css',
-    './css/open-iconic/font/css/open-iconic-bootstrap.min.css',
-    //'./open-iconic',
-    './css/open-iconic/font/fonts/open-iconic.woff',
+    '/index.html',
+    '/css/site.css',
+    '/css/bootstrap/bootstrap.min.css',
+    '/css/open-iconic/font/css/open-iconic-bootstrap.min.css',
+    '/open-iconic',
+    '/css/open-iconic/font/fonts/open-iconic.woff',
     //'/css/loading.css',
-    './sample-data/weather.json',
+    '/sample-data/weather.json',
     //Blazor framework
-    './_framework/blazor.webassembly.js',
-    './_framework/blazor.boot.json',
-    './_framework/blazor.server.js',
+    '/_framework/blazor.webassembly.js',
+    '/_framework/blazor.boot.json',
+    '/_framework/blazor.server.js',
     //Our additional files
-    './manifest.json',
-    './serviceworker.js',
-    './icons/icon-192x192.png',
-    './icons/icon-512x512.png',
+    '/manifest.json',
+    '/serviceworker.js',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
     //Content
-    //'/ubiquitous-memory/_content',
-    //'/ubiquitous-memory/_content/Blazored.LocalStorage',
-    './_content/Blazored.LocalStorage/blazored.LocalStorage.js',
-    './_content/Blazored.LocalStorage/blazored.LocalStorage.js.map',
-    './_content/Blazored.LocalStorage/blazored.LocalStorage.ts',
+    '/_content',
+    '/_content/Blazored.LocalStorage',
+    '/_content/Blazored.LocalStorage/blazored.LocalStorage.js',
+    '/_content/Blazored.LocalStorage/blazored.LocalStorage.js.map',
+    '/_content/Blazored.LocalStorage/blazored.LocalStorage.ts',
     //The web assembly/.net dll's
-    './_framework/wasm/mono.js',
-    './_framework/wasm/mono.wasm',
-    './_framework/_bin/Microsoft.AspNetCore.Components.Browser.dll',
-    './_framework/_bin/Microsoft.AspNetCore.Components.dll',
-    './_framework/_bin/Microsoft.AspNetCore.Blazor.dll',
-    './_framework/_bin/Microsoft.Extensions.DependencyInjection.Abstractions.dll',
-    './_framework/_bin/Microsoft.Extensions.DependencyInjection.dll',
-    './_framework/_bin/Microsoft.JSInterop.dll',
-    './_framework/_bin/Mono.Security.dll',
-    './_framework/_bin/Mono.WebAssembly.Interop.dll',
-    './_framework/_bin/mscorlib.dll',
-    './_framework/_bin/System.ComponentModel.Annotations.dll',
-    './_framework/_bin/System.Core.dll',
-    './_framework/_bin/System.dll',
-    './_framework/_bin/System.Net.Http.dll',
-    './_framework/_bin/Blazored.LocalStorage.dll',
+    '/_framework/wasm/mono.js',
+    '/_framework/wasm/mono.wasm',
+    '/_framework/_bin/Microsoft.AspNetCore.Components.Browser.dll',
+    '/_framework/_bin/Microsoft.AspNetCore.Components.dll',
+    '/_framework/_bin/Microsoft.AspNetCore.Blazor.dll',
+    '/_framework/_bin/Microsoft.Extensions.DependencyInjection.Abstractions.dll',
+    '/_framework/_bin/Microsoft.Extensions.DependencyInjection.dll',
+    '/_framework/_bin/Microsoft.JSInterop.dll',
+    '/_framework/_bin/Mono.Security.dll',
+    '/_framework/_bin/Mono.WebAssembly.Interop.dll',
+    '/_framework/_bin/mscorlib.dll',
+    '/_framework/_bin/System.ComponentModel.Annotations.dll',
+    '/_framework/_bin/System.Core.dll',
+    '/_framework/_bin/System.dll',
+    '/_framework/_bin/System.Net.Http.dll',
+    '/_framework/_bin/Blazored.LocalStorage.dll',
     //Pages
-    //'/ubiquitous-memory/counter',
-    //'/ubiquitous-memory/todo',
-    //'/ubiquitous-memory/fetchdata',
+    '/counter',
+    '/todo',
+    '/fetchdata',
     //The compiled project .dll's
-    './_framework/_bin/BlazorDemo.dll',
-    './_framework/_bin/BlazorDemo.pdb'
+    '/_framework/_bin/BlazorDemo.dll',
+    '/_framework/_bin/BlazorDemo.pdb'
 ];
 
 self.addEventListener('install', function (e) {
@@ -57,15 +57,7 @@ self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(cacheName).then(function (cache) {
             console.log('[ServiceWorker] Caching app shell');
-            
-			
-			
-			return cache.addAll(filesToCache)
-			.then(function(out){
-				console.log(out);
-			}).catch(function(err){
-				console.log(err);
-			});
+            return cache.addAll(filesToCache);
         })
     );
 });
